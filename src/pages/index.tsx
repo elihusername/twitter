28b8f9504e9506ac1aba51af6287887c2b5fc38a
ElihuDiaz <diazelihu96@gmail.com>
2023-04-03 18:27:16 -0400

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
   return (
     <>
       <Head>
@@ -17,6 +19,11 @@ const Home: NextPage = () => {
         <div>
           {!user.isSignedIn && <SignInButton />}
           {!!user.isSignedIn && <SignOutButton />}
+        </div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
         </div>
       </main>
     </>
